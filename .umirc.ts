5bea5ecd0d7f111e95278173656f6e9a9e61ed1f
yuqing521 <1581446178@qq.com>
2021-03-20 19:48:59 +0800

    update icon

--- .umirc.ts
+++ .umirc.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'yu-components',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: 'https://i.loli.net/2021/03/20/LnQfRSAb61XsZ98.png',
+  logo: 'https://i.loli.net/2021/03/20/LnQfRSAb61XsZ98.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
 });
